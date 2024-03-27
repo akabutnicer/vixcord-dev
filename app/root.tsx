409d5551af5e273361c2c6f73dfa44900e42a725
Aka, but nice <120267628+akabutnicer@users.noreply.github.com>
2024-03-26 20:23:08 -0400

    Update root.tsx

--- app/root.tsx
+++ app/root.tsx
@@ -5,7 +5,11 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import stylesheet from "~/tailwind.css?url";
 
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: stylesheet },
+];
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
