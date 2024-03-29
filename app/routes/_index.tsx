cb1e6ac4761038b12725ce8c65a0f05b278015ed
Aka, but nice <120267628+akabutnicer@users.noreply.github.com>
2024-03-29 14:20:16 -0400

    Update _index.tsx

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -67,13 +67,13 @@ export default function Index() {
               >
                 Terms of conditions
               </button>
-              <button
-                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+               <button
+                data-modal-target="default-modal"
+                data-modal-toggle="default-modal"
+                class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-70"
                 type="button"
-                data-modal-target="timeline-modal"
-                data-modal-toggle="timeline-modal"
               >
-                Catalog
+                 Catalog
               </button>
             </div>
           </div>
