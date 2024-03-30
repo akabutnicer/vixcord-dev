f66df3ab5322543e6339a753d5e47537f8b5c8f6
Aka, but nice <120267628+akabutnicer@users.noreply.github.com>
2024-03-30 18:26:29 -0400

    Update _index.tsx

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -10,17 +10,35 @@ export const meta: MetaFunction = () => {
   ];
 };
 
-function Title({ title, halfway = false }) {
-  var halfwayText = (
+function Title({ title, split = false }) {
+  var splitText = (
     <>
-      <span class="text-white">{title.slice(0, title.length / 2)}</span>
+      <span class="text-gray-900 dark:text-white">{title.slice(0, title.length / 2)}</span>
       {<span class="text-blue-700">{title.slice(title.length / 2)}</span>}
     </>
   );
   return (
-    <div class="absolute left-20 top-5">
-      <h1 class="text-4xl font-extrabold">{halfway ? halfwayText : title}</h1>
+   
+
+<div id="marketing-banner" tabindex="-1" class="fixed z-50 flex flex-col md:flex-row justify-between w-[calc(100%-2rem)] p-4 -translate-x-1/2 bg-white border border-gray-100 rounded-lg shadow-sm lg:max-w-7xl left-1/2 top-6 dark:bg-gray-700 dark:border-gray-600">
+    <div class="flex flex-col items-start mb-3 me-4 md:items-center md:flex-row md:mb-0">
+        <a href="https://flowbite.com/" class="flex items-center mb-2 border-gray-200 md:pe-4 md:me-4 md:border-e md:mb-0 dark:border-gray-600">
+            <span class="self-center text-lg font-semibold whitespace-nowrap dark:text-white">{split ? splitText : title}</span>
+        </a>
+        <p class="flex items-center text-sm font-normal text-gray-500 dark:text-gray-400">Have everlasting fun with your friends on Vixcord!</p>
     </div>
+    <div class="flex items-center flex-shrink-0">
+        <a href="#" class="px-5 py-2 me-2 text-xs font-medium text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Sign up</a>
+        <button data-dismiss-target="#marketing-banner" type="button" class="flex-shrink-0 inline-flex justify-center w-7 h-7 items-center text-gray-400 hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 dark:hover:bg-gray-600 dark:hover:text-white">
+            <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+            </svg>
+            <span class="sr-only">Close banner</span>
+        </button>
+    </div>
+</div>
+
+
   );
 }
 export default function Index() {
@@ -41,7 +59,7 @@ var [catalog, setCatalog] = useState(<div />);
   }, []);
   return (
     <>
-      <Title title="Vixcord" halfway={true} />
+      <Header title="Vixcord" halfway={true} />
       <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
         <section class="dark:bg-gray-900">
           <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
